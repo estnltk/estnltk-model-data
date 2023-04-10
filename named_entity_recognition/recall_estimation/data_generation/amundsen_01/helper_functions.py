a5f709dbaa693d0c26d99984fc519e840566f0cd
rasmusmaide <36761599+rasmusmaide@users.noreply.github.com>
2023-04-10 13:45:54 +0300

    Add helper functions

---
+++ named_entity_recognition/recall_estimation/data_generation/amundsen_01/helper_functions.py
@@ -0,0 +1,97 @@
+import configparser
+import os
+from estnltk.storage.postgres import PostgresStorage
+
+
+def load_configuration(config_file):
+    config = configparser.ConfigParser()
+
+    file_name = os.path.abspath(os.path.expanduser(os.path.expandvars(str(config_file))))
+
+    if not os.path.exists(file_name):
+        raise ValueError("File {file} does not exist".format(file=str(config_file)))
+
+    if len(config.read(file_name)) != 1:
+        raise ValueError("File {file} is not accessible or is not in valid INI format".format(file=config_file))
+
+    for option in ["host", "port", "database", "username", "password", "role", "schema", "collection"]:
+        if not config.has_option('source_database', option):
+            prelude = "Error in file {}\n".format(file_name) if len(file_name) > 0 else ""
+            raise ValueError(
+                "{prelude}Missing option {option} in the section [{section}]".format(
+                    prelude=prelude, option=option, section='source_database'
+                )
+            )
+
+    config.read(file_name)
+    
+    return config
+
+
+def connect_to_database(config):
+
+    dbname = config['source_database']['database']
+    user = config['source_database']['username']
+    password = config['source_database']['password']
+    host = config['source_database']['host']
+    port = config['source_database']['port']
+    role = config['source_database']['role']
+    schema = config['source_database']['schema']
+    collection = config['source_database']['collection']
+
+
+    storage = PostgresStorage(host=host,
+                              port=int(port),
+                              dbname=dbname,
+                              user=user,
+                              password=password,
+                              schema=schema,
+                              role=role,
+                              temporary=False)
+    return storage
+
+def load_local_configuration(config_file):
+    
+    config = configparser.ConfigParser()
+
+    file_name = os.path.abspath(os.path.expanduser(os.path.expandvars(str(config_file))))
+
+    if not os.path.exists(file_name):
+        raise ValueError("File {file} does not exist".format(file=str(config_file)))
+
+    if len(config.read(file_name)) != 1:
+        raise ValueError("File {file} is not accessible or is not in valid INI format".format(file=config_file))
+
+    for option in ["host", "port", "database", "username", "password", "schema", "collection"]:
+        if not config.has_option('target_database', option):
+            prelude = "Error in file {}\n".format(file_name) if len(file_name) > 0 else ""
+            raise ValueError(
+                "{prelude}Missing option {option} in the section [{section}]".format(
+                    prelude=prelude, option=option, section='target_database'
+                )
+            )
+
+    config.read(file_name)
+    
+    return config
+
+def connect_to_local_database(config):
+    
+    dbname = config['target_database']['database']
+    user = config['target_database']['username']
+    password = config['target_database']['password']
+    host = config['target_database']['host']
+    port = config['target_database']['port']
+    schema = config['target_database']['schema']
+    collection = config['target_database']['collection']
+
+    localstorage = PostgresStorage(host=host,
+                              port=int(port),
+                              dbname=dbname,
+                              user=user,
+                              password=password,
+                              schema=schema,
+                              role=None,
+                              temporary=False)
+    
+    return localstorage
